f8bc94ebbbc1c7d7932707069cb7cb3a53aa30a4
Edgarbriceno26@outlook.es <Edgarbriceno26@outlook.es>
2025-08-16 16:23:45 -0400

    mejora estructura del proyecto

---
+++ lib/app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:memorizacion_app/pages/home_page.dart';
+
+class MemoApp extends StatelessWidget {
+  const MemoApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Memorizacion Demo',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
